b6621d0fc0e312f333cd50ea8cbf8808bac2765d
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-19 06:02:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 // Add your functions here
 
-const map = (sourceArray, fn)=>{
+const map = (array, fn)=>{
   let newArray = [];
-  for (let i = 0; i < sourceArray.length; i++) {
-    let element = sourceArray[i];
+  for (let i = 0; i < array.length; i++) {
+    let element = array[i];
     newArray.push(fn(element));
   }
   return newArray;
 };
 
-const reduce = (sourceArray, fn, begin)=>{
+const reduce = (array, fn, begin)=>{
   if (begin) {
       let entire = begin;
-      for (let i = 0; i < sourceArray.length; i++) {
-        let element = sourceArray[i];
+      for (let i = 0; i < array.length; i++) {
+        let element = array[i];
         entire = fn(entire, element);
       }
       return entire;
   }
   else {
-      let total = sourceArray[0];
-      for (let i = 1; i < sourceArray.length; i++) {
-        let element = sourceArray[i];
+      let total = array[0];
+      for (let i = 1; i < array.length; i++) {
+        let element = array[i];
         total = fn(total, element);
       }
       return total;
