c2b50a0259443db68403d6a4d771ac86837b16cf
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-19 05:55:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 // Add your functions here
 
-const map = (sourceArray, callBackFunc)=>{
+const map = (array, callBackFunc)=>{
   let newArray = [];
-  for (let i = 0; i < sourceArray.length; i++) {
-    let element = sourceArray[i];
+  for (let i = 0; i < array.length; i++) {
+    let element = array[i];
     newArray.push(callBackFunc(element));
   }
   return newArray;
 };
 
-const reduce = (sourceArray, callBackFunc, begin)=>{
+const reduce = (array, callBackFunc, begin)=>{
   if (begin) {
       let entire = begin;
-      for (let i = 0; i < sourceArray.length; i++) {
-        let element = sourceArray[i];
+      for (let i = 0; i < array.length; i++) {
+        let element = array[i];
         entire = callBackFunc(entire, element);
       }
       return entire;
   }
   else {
-      let total = sourceArray[0];
-      for (let i = 1; i < sourceArray.length; i++) {
-        let element = sourceArray[i];
+      let total = array[0];
+      for (let i = 1; i < arraylength; i++) {
+        let element = array[i];
         total = callBackFunc(total, element);
       }
       return total;
